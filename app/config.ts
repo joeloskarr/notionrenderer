98662882216fe7f90f31c0bfac173602034ff572
joeloskarr <oskar.raisanen@gmail.com>
2025-04-05 21:31:20 +0000

    vercel2

--- app/config.ts
+++ app/config.ts
@@ -2,6 +2,6 @@
 export const isDev =
     process.env.NODE_ENV === 'development' || !process.env.NODE_ENV
 
-export const server = isDev ? process.env.API_PATH : "[YOUR PUBLIC URL]";
-export const serviceUrl = isDev ? process.env.HOST : "[YOUR PUBLIC URL]";
+export const server = isDev ? process.env.API_PATH : "https://notionrenderer.vercel.app/api/";
+export const serviceUrl = isDev ? process.env.HOST : "https://notionrenderer.vercel.app/pages/";
 export const rootNotionId = "1c4aaa7b984780bc9812fc34540239f5"
